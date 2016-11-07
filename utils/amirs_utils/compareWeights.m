c2469c2a66b50a3d82b10850a976f374d3403c98
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-07 15:18:31 -0500

    update

--- utils/amirs_utils/compareWeights.m
+++ utils/amirs_utils/compareWeights.m
@@ -1,7 +1,7 @@
-% networkArch = 'cifar-lenet';
-% layer = 4; % [1, 4, 7]
-networkArch = 'cifar-alexnet';
-layer = 1; % [1, 3, 6, 9, 11]
+networkArch = 'cifar-lenet';
+layer = 4; % [1, 4, 7]
+% networkArch = 'cifar-alexnet';
+% layer = 1; % [1, 3, 6, 9, 11]
 
 file_name = sprintf('W1-layer-%d.mat', layer);
 devPath = getDevPath();
@@ -10,6 +10,7 @@ w_baseline = load(fullfile(devPath, 'data', networkArch, 'w_baseline', file_name
 w_compRand = load(fullfile(devPath, 'data', networkArch, 'w_compRand', file_name));
 w_1D = load(fullfile(devPath, 'data', networkArch, 'w_1D', file_name));
 w_2D_mult = load(fullfile(devPath, 'data', networkArch, 'w_2D-mult', file_name));
+w_2D_mult2 = load(fullfile(devPath, 'data', networkArch, 'w_2D-mult2', file_name));
 w_2D_super = load(fullfile(devPath, 'data', networkArch, 'w_2D-super', file_name));
 w_2D_posneg = load(fullfile(devPath, 'data', networkArch, 'w_2D-posneg', file_name));
 w_2D_positive = load(fullfile(devPath, 'data', networkArch, 'w_2D-positive', file_name));
@@ -19,6 +20,7 @@ w_baseline = w_baseline.W1;
 w_compRand = w_compRand.W1;
 w_1D = w_1D.W1;
 w_2D_mult = w_2D_mult.W1;
+w_2D_mult2 = w_2D_mult2.W1;
 w_2D_super = w_2D_super.W1;
 w_2D_posneg = w_2D_posneg.W1;
 w_2D_positive = w_2D_positive.W1;
@@ -27,13 +29,14 @@ w_2D_shiftflip = w_2D_shiftflip.W1;
 assert(logical(prod(size(w_baseline) == size(w_compRand))));
 assert(logical(prod(size(w_baseline) == size(w_1D))));
 assert(logical(prod(size(w_baseline) == size(w_2D_mult))));
+assert(logical(prod(size(w_baseline) == size(w_2D_mult2))));
 assert(logical(prod(size(w_baseline) == size(w_2D_super))));
 assert(logical(prod(size(w_baseline) == size(w_2D_posneg))));
 assert(logical(prod(size(w_baseline) == size(w_2D_positive))));
 assert(logical(prod(size(w_baseline) == size(w_2D_shiftflip))));
 
 % randomly choose 3 kernels to compare
-num_kernels = 8;
+num_kernels = 9;
 for k = 1:2
   aa = size(w_baseline, 1);
   bb = size(w_baseline, 2);
@@ -62,6 +65,10 @@ for k = 1:2
   w_2D_mult_slice = w_2D_mult(:, :, cc, dd);
   subplot(2,num_kernels,i), imshow(w_2D_mult_slice, []), title('2D mult');
   subplot(2,num_kernels,num_kernels + i), mesh(1:1:aa, 1:1:aa, w_2D_mult_slice);
+  i = i + 1;
+  w_2D_mult2_slice = w_2D_mult2(:, :, cc, dd);
+  subplot(2,num_kernels,i), imshow(w_2D_mult2_slice, []), title('2D mult 2');
+  subplot(2,num_kernels,num_kernels + i), mesh(1:1:aa, 1:1:aa, w_2D_mult2_slice);
   i = i + 1;
   w_2D_super_slice = w_2D_super(:, :, cc, dd);
   subplot(2,num_kernels,i), imshow(w_2D_super_slice, []), title('2D super');
