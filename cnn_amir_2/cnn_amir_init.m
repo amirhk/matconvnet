b52fb01d0d7c6521f1b81e477973bdb5c81433b0
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2016-11-10 00:04:24 -0500

    update

--- cnn_amir_2/cnn_amir_init.m
+++ cnn_amir_2/cnn_amir_init.m
@@ -14,7 +14,8 @@ net.layers = {};
 % Meta parameters
 switch opts.networkArch
   case 'lenet'
-    net.meta.trainOpts.learningRate = [0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5)];
+    net.meta.trainOpts.learningRate = [0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5) 0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5) 0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5)];
+    % net.meta.trainOpts.learningRate = [0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,20)  0.01*ones(1,100)];
   case 'alexnet'
     net.meta.trainOpts.learningRate = [0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5)];
   case 'alexnet-bnorm'
