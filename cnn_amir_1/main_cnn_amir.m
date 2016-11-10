e1dee5c125d277e2caf2eb1889322d59335e7c6b
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-10 12:06:18 -0500

    udpate

--- cnn_amir_1/main_cnn_amir.m
+++ cnn_amir_1/main_cnn_amir.m
@@ -8,13 +8,13 @@ function main_cnn_amir(varargin)
 % -- ==                                                                   -- ==
 % -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- ==
 
-  % networkArch = 'lenet';
+  networkArch = 'lenet';
   % backpropDepthList = [13, 10, 7, 4];
-  % backpropDepthList = [13];
+  backpropDepthList = [13];
 
-  networkArch = 'alexnet';
-  % backpropDepthList = [20, 18, 15, 12, 10, 7];
-  backpropDepthList = [20];
+  % networkArch = 'alexnet';
+  % % backpropDepthList = [20, 18, 15, 12, 10, 7];
+  % backpropDepthList = [20];
 
   % networkArch = 'alexnet-bnorm';
   % % backpropDepthList = [20, 18, 15, 12, 10, 7];
@@ -31,7 +31,7 @@ function main_cnn_amir(varargin)
 % -- ==                                                                   -- ==
 % -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- ==
 
-  dataset = 'stl-10'; % {'cifar' | 'stl-10'}
+  dataset = 'cifar'; % {'cifar' | 'stl-10'}
   weightInitSource = 'gen';  % {'load' | 'gen'}
 
   % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
