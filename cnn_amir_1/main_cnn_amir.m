df242249850bbcb25a4fefd2877c6ada04dfbe73
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-10 16:36:57 -0500

    update

--- cnn_amir_1/main_cnn_amir.m
+++ cnn_amir_1/main_cnn_amir.m
@@ -32,12 +32,12 @@ function main_cnn_amir(varargin)
 % -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- ==
 
   dataset = 'cifar'; % {'cifar' | 'stl-10'}
-  weightInitSource = 'gen';  % {'load' | 'gen'}
+  % weightInitSource = 'gen';  % {'load' | 'gen'}
   % weightInitSequenceList = {{'compRand', 'compRand', 'compRand', 'compRand', 'compRand'}};
-  weightInitSequenceList = {{'2D-shiftflip', '2D-shiftflip', '1D', '1D', '1D'}};
 
-  % weightInitSource = 'load';  % {'load' | 'gen'}
-  % % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
+  weightInitSource = 'load';  % {'load' | 'gen'}
+  % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
+  weightInitSequenceList = {{'2D-shiftflip', '2D-shiftflip', '1D', '1D', '1D'}};
   % weightInitSequenceList = { ...
   %   {'1D', '1D', '1D', '1D', '1D'}, ...
   %   {'2D-shiftflip', '2D-shiftflip', '1D', '1D', '1D'}, ...
