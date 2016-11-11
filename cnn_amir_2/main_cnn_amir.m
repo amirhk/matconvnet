a111db494f0c421cb806152efb92f86e34d6364a
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-10 20:21:22 -0500

    update

--- cnn_amir_2/main_cnn_amir.m
+++ cnn_amir_2/main_cnn_amir.m
@@ -9,8 +9,8 @@ function main_cnn_amir(varargin)
 % -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- ==
 
   networkArch = 'lenet';
-  % backpropDepthList = [13, 10, 7, 4];
-  backpropDepthList = [13];
+  backpropDepthList = [13, 10, 7, 4];
+  % backpropDepthList = [13];
 
   % networkArch = 'alexnet';
   % % backpropDepthList = [20, 18, 15, 12, 10, 7];
@@ -32,12 +32,12 @@ function main_cnn_amir(varargin)
 % -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- ==
 
   dataset = 'stl-10'; % {'cifar' | 'stl-10'}
-  weightInitSource = 'gen';  % {'load' | 'gen'}
-  weightInitSequenceList = {{'compRand', 'compRand', 'compRand', 'compRand', 'compRand'}};
+  % weightInitSource = 'gen';  % {'load' | 'gen'}
+  % weightInitSequenceList = {{'compRand', 'compRand', 'compRand', 'compRand', 'compRand'}};
 
-  % weightInitSource = 'load';  % {'load' | 'gen'}
-  % % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
-  % weightInitSequenceList = {{'1D', '1D', '1D', '1D', '1D'}};
+  weightInitSource = 'load';  % {'load' | 'gen'}
+  % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
+  weightInitSequenceList = {{'baseline', 'baseline', 'baseline', 'baseline', 'baseline'}};
   % weightInitSequenceList = {{'2D-shiftflip', '2D-shiftflip', '1D', '1D', '1D'}};
   % weightInitSequenceList = {{'2D-mult-randn', '2D-mult-randn', 'compRand', 'compRand', 'compRand'}};
   % weightInitSequenceList = { ...
