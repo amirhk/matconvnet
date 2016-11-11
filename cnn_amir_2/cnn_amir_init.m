6cb07ec650149367ed7da316eef923e4e099ed7f
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-10 20:19:57 -0500

    update

--- cnn_amir_2/cnn_amir_init.m
+++ cnn_amir_2/cnn_amir_init.m
@@ -16,14 +16,10 @@ switch opts.networkArch
   case 'lenet'
     switch opts.dataset
       case 'cifar'
-        % net.meta.trainOpts.learningRate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
-        net.meta.trainOpts.learningRate = [0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,10)]; % imdb-javad
+        net.meta.trainOpts.learningRate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
       case 'stl-10'
         % net.meta.trainOpts.learningRate = [0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5)]; % imdb-amir
         net.meta.trainOpts.learningRate = [0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,10)]; % imdb-javad
-        % net.meta.trainOpts.learningRate = [0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,30)];
-        % net.meta.trainOpts.learningRate = [0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,10) 0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,10)];
-        % net.meta.trainOpts.learningRate = [ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.05 0.05*ones(1,60)];
     end
   case 'alexnet'
     net.meta.trainOpts.learningRate = [0.01*ones(1,5) 0.005*ones(1,25) 0.001*ones(1,10) 0.0005*ones(1,5) 0.0001*ones(1,5)];
