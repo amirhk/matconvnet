941d3d860c1bcc2fc7ee11a415deb943c73c0acb
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-13 17:02:06 -0500

    update

--- cnn_amir_1/main_cnn_amir.m
+++ cnn_amir_1/main_cnn_amir.m
@@ -32,14 +32,12 @@ function main_cnn_amir(varargin)
 % -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- == -- ==
 
   dataset = 'stl-10'; % {'cifar' | 'stl-10'}
-  weightInitSource = 'gen';  % {'load' | 'gen'}
-  weightInitSequenceList = {{'compRand', 'compRand', 'compRand', 'compRand', 'compRand'}};
+  % weightInitSource = 'gen';  % {'load' | 'gen'}
+  % weightInitSequenceList = {{'compRand', 'compRand', 'compRand', 'compRand', 'compRand'}};
 
-  % weightInitSource = 'load';  % {'load' | 'gen'}
-  % % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
-  % % weightInitSequenceList = {{'baseline', 'baseline', 'baseline', 'baseline', 'baseline'}};
-  % % weightInitSequenceList = {{'2D-shiftflip', '2D-shiftflip', '1D', '1D', '1D'}};
-  % weightInitSequenceList = {{'2D-mult-randn', '2D-mult-randn', 'compRand', 'compRand', 'compRand'}};
+  weightInitSource = 'load';  % {'load' | 'gen'}
+  % weightInitTypes: {'baseline', 'compRand', '1D', '2D-positive', '2D-super', '2D-posneg', '2D-shiftflip', '2D-mult-randn', '2D-mult-kernel'};
+  weightInitSequenceList = {{'baseline', 'baseline', 'baseline', 'baseline', 'baseline'}};
   % weightInitSequenceList = { ...
   %   {'1D', '1D', '1D', '1D', '1D'}, ...
   %   {'2D-shiftflip', '2D-shiftflip', '1D', '1D', '1D'}, ...
