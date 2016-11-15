fcd1c4e9ea812d2f256f02deee231b4defcb5824
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2016-11-14 23:05:59 -0500

    update

--- cnn_amir_1/cnn_amir_init.m
+++ cnn_amir_1/cnn_amir_init.m
@@ -18,7 +18,7 @@ switch opts.networkArch
       case 'cifar'
         % net.meta.trainOpts.learningRate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
         % net.meta.trainOpts.learningRate = [0.005*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
-        net.meta.trainOpts.learningRate = [0.1*ones(1,15)];
+        net.meta.trainOpts.learningRate = [0.00001*ones(1,15)];
       case 'stl-10'
         net.meta.trainOpts.learningRate = [0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.06 0.05*ones(1,10)]; % javad-LR
     end
