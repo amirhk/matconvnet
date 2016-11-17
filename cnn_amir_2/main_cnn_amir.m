1d90de13aa6966a2660887c49f8d1928a90492f1
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-17 12:17:26 -0500

    update

--- cnn_amir_2/main_cnn_amir.m
+++ cnn_amir_2/main_cnn_amir.m
@@ -17,11 +17,11 @@ function main_cnn_amir(varargin)
 
   networkArch = 'lenet';
   % backpropDepthList = [13, 10, 7, 4]; % no dropout
-  % backpropDepthList = [14, 10, 7, 4]; % 1 x dropout after 1st layer
+  backpropDepthList = [14, 10, 7, 4]; % 1 x dropout after 1st layer
   % backpropDepthList = [14, 11, 8, 4]; % 1 x dropout after 3rd layer
   % backpropDepthList = [15, 11, 8, 4]; % 2 x dropout after 1st and 3rd layers
   % backpropDepthList = [13];
-  backpropDepthList = [14];
+  % backpropDepthList = [14];
 
   % networkArch = 'lenet';
   % % backpropDepthList = [13, 10, 7, 4];
