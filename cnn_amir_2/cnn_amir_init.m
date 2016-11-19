1845d8ea468bbb5bc7d411dded7dccb7c7af5730
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-11-18 19:05:18 -0500

    update

--- cnn_amir_2/cnn_amir_init.m
+++ cnn_amir_2/cnn_amir_init.m
@@ -115,8 +115,8 @@ switch opts.networkArch
 
     % --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
     layerNumber = layerNumber + 2;
-    net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 1, 64, 10, 5/100, 0, 'compRand', 'gen');
-    % net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 1, 64, 100, 5/100, 0, 'compRand', 'gen');
+    % net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 1, 64, 10, 5/100, 0, 'compRand', 'gen');
+    net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 1, 64, 100, 5/100, 0, 'compRand', 'gen');
 
     % --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
     % Loss layer
