4fc7d8e47101919deb20aebfb47f74ef68e4d232
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2016-11-20 20:46:31 -0500

    added code for mnist to run on both mnistnet and lenet

--- utils/amirs_utils/constructMnistImdb.m
+++ utils/amirs_utils/constructMnistImdb.m
@@ -1,52 +1,61 @@
 % --------------------------------------------------------------------
 function imdb = constructMnistImdb(opts)
 % --------------------------------------------------------------------
-% Preapre the imdb structure, returns image data with mean image subtracted
-files = {'train-images-idx3-ubyte', ...
-         'train-labels-idx1-ubyte', ...
-         't10k-images-idx3-ubyte', ...
-         't10k-labels-idx1-ubyte'} ;
+  fprintf('[INFO] Constructing STL-10 imdb...');
+  % Preapre the imdb structure, returns image data with mean image subtracted
+  files = {'train-images-idx3-ubyte', ...
+           'train-labels-idx1-ubyte', ...
+           't10k-images-idx3-ubyte', ...
+           't10k-labels-idx1-ubyte'} ;
 
-if ~exist(opts.dataDir, 'dir')
-  mkdir(opts.dataDir) ;
-end
-
-for i=1:4
-  if ~exist(fullfile(opts.dataDir, files{i}), 'file')
-    url = sprintf('http://yann.lecun.com/exdb/mnist/%s.gz',files{i}) ;
-    fprintf('downloading %s\n', url) ;
-    gunzip(url, opts.dataDir) ;
+  if ~exist(opts.dataDir, 'dir')
+    mkdir(opts.dataDir) ;
   end
-end
 
-f=fopen(fullfile(opts.dataDir, 'train-images-idx3-ubyte'),'r') ;
-x1=fread(f,inf,'uint8');
-fclose(f) ;
-x1=permute(reshape(x1(17:end),28,28,60e3),[2 1 3]) ;
+  for i=1:4
+    if ~exist(fullfile(opts.dataDir, files{i}), 'file')
+      url = sprintf('http://yann.lecun.com/exdb/mnist/%s.gz',files{i}) ;
+      fprintf('downloading %s\n', url) ;
+      gunzip(url, opts.dataDir) ;
+    end
+  end
 
-f=fopen(fullfile(opts.dataDir, 't10k-images-idx3-ubyte'),'r') ;
-x2=fread(f,inf,'uint8');
-fclose(f) ;
-x2=permute(reshape(x2(17:end),28,28,10e3),[2 1 3]) ;
+  f=fopen(fullfile(opts.dataDir, 'train-images-idx3-ubyte'),'r') ;
+  x1=fread(f,inf,'uint8');
+  fclose(f) ;
+  x1=permute(reshape(x1(17:end),28,28,60e3),[2 1 3]) ;
 
-f=fopen(fullfile(opts.dataDir, 'train-labels-idx1-ubyte'),'r') ;
-y1=fread(f,inf,'uint8');
-fclose(f) ;
-y1=double(y1(9:end)')+1 ;
+  f=fopen(fullfile(opts.dataDir, 't10k-images-idx3-ubyte'),'r') ;
+  x2=fread(f,inf,'uint8');
+  fclose(f) ;
+  x2=permute(reshape(x2(17:end),28,28,10e3),[2 1 3]) ;
 
-f=fopen(fullfile(opts.dataDir, 't10k-labels-idx1-ubyte'),'r') ;
-y2=fread(f,inf,'uint8');
-fclose(f) ;
-y2=double(y2(9:end)')+1 ;
+  f=fopen(fullfile(opts.dataDir, 'train-labels-idx1-ubyte'),'r') ;
+  y1=fread(f,inf,'uint8');
+  fclose(f) ;
+  y1=double(y1(9:end)')+1 ;
 
-set = [ones(1,numel(y1)) 3*ones(1,numel(y2))];
-data = single(reshape(cat(3, x1, x2),28,28,1,[]));
-dataMean = mean(data(:,:,:,set == 1), 4);
-data = bsxfun(@minus, data, dataMean) ;
+  f=fopen(fullfile(opts.dataDir, 't10k-labels-idx1-ubyte'),'r') ;
+  y2=fread(f,inf,'uint8');
+  fclose(f) ;
+  y2=double(y2(9:end)')+1 ;
 
-imdb.images.data = data ;
-imdb.images.data_mean = dataMean;
-imdb.images.labels = cat(2, y1, y2) ;
-imdb.images.set = set ;
-imdb.meta.sets = {'train', 'val', 'test'} ;
-imdb.meta.classes = arrayfun(@(x)sprintf('%d',x),0:9,'uniformoutput',false) ;
+  set = [ones(1,numel(y1)) 3*ones(1,numel(y2))];
+  if strcmp(opts.networkArch, 'mnistnet')
+    data = single(reshape(cat(3, x1, x2),28,28,1,[]));
+  elseif strcmp(opts.networkArch, 'lenet')
+    % MNIST is single channel of size 28x28.. for LeNet, repmat channel 3 and pad
+    data = single(padarray(repmat(reshape(cat(3, x1, x2),28,28,1,[]), [1,1,3,1]), [2,2]));
+  else
+    disp('wtf!!')
+  end
+  dataMean = mean(data(:,:,:,set == 1), 4);
+  data = bsxfun(@minus, data, dataMean) ;
+
+  imdb.images.data = data ;
+  imdb.images.data_mean = dataMean;
+  imdb.images.labels = cat(2, y1, y2) ;
+  imdb.images.set = set ;
+  imdb.meta.sets = {'train', 'val', 'test'} ;
+  imdb.meta.classes = arrayfun(@(x)sprintf('%d',x),0:9,'uniformoutput',false) ;
+  fprintf('done!\n\n');
