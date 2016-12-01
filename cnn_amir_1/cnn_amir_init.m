d247218fdbf3fad44f5b5db852592ea7c8d93437
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-12-01 13:57:31 -0500

    update

--- cnn_amir_1/cnn_amir_init.m
+++ cnn_amir_1/cnn_amir_init.m
@@ -21,7 +21,7 @@ switch opts.networkArch
         % net.meta.trainOpts.learningRate = [0.01*ones(1,50) 0.005*ones(1,50) 0.001*ones(1,100) 0.0005*ones(1,100)]; % matconvnet default
         % net.meta.trainOpts.learningRate = [0.005*ones(1,40) 0.001*ones(1,50) 0.0005*ones(1,110)]; % matconvnet default
         % net.meta.trainOpts.learningRate = [0.005*ones(1,40) 0.001*ones(1,50)];
-        net.meta.trainOpts.learningRate = [0.005*ones(1,10) 0.001*ones(1,10)];
+        net.meta.trainOpts.learningRate = [0.001*ones(1,10) 0.001*ones(1,20)];
     end
   case 'mnistnet'
     switch opts.dataset
@@ -74,6 +74,7 @@ switch opts.networkArch
     % --- --- ---                                                     --- --- --
     % --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --
     layerNumber = 1;
+    % net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 5, 4, 32, 1/100, 2, char(opts.weightInitSequence{1}), 'gen');
     net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 5, 4, 32, 1/100, 2, char(opts.weightInitSequence{1}), opts.weightInitSource);
     % net.layers{end+1} = convLayer(opts.dataset, opts.networkArch, layerNumber, 5, 8, 32, 1/100, 2, char(opts.weightInitSequence{1}), opts.weightInitSource);
     net.layers{end+1} = poolingLayerLeNetMax(layerNumber);
