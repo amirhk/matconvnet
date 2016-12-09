5dd518aa9892b5c375f3ce966d20b882dea9f554
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-12-09 11:46:40 -0500

    update

--- cnn_amir_1/cnn_amir_init.m
+++ cnn_amir_1/cnn_amir_init.m
@@ -63,7 +63,7 @@ net.meta.trainOpts.backpropDepth = opts.backpropDepth;
 net.meta.trainOpts.numEpochs = numel(net.meta.trainOpts.learningRate);
 net.meta.inputSize = [32 32 3];
 net.meta.trainOpts.weightDecay = opts.weightDecay;
-net.meta.trainOpts.batchSize = 250;
+net.meta.trainOpts.batchSize = 100;
 opts = vl_argparse(opts, varargin);
 
 switch opts.networkArch
