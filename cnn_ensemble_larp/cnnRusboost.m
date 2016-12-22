276ba4e0d9c5c9833841f3a1add4699a16aca00e
Amir <a6karimi@v1020-wn-160-219.campus-dynamic.uwaterloo.ca>
2016-12-22 16:15:58 -0500

    update

--- cnn_ensemble_larp/cnnRusboost.m
+++ cnn_ensemble_larp/cnnRusboost.m
@@ -245,8 +245,8 @@ function [ensemble_models_info, weighted_results] = mainCNNRusboost(single_ensem
     train_opts.weight_init_sequence = opts.weight_init_sequence;
     train_opts.debug_flag = false;
     train_opts.experiment_parent_dir = opts.experiment_dir_path;
-    train_opts.gpus = [1];
-    train_opts.backprop_depth = 4;
+    train_opts.gpus = [2];
+    train_opts.backprop_depth = 13;
     [net, info] = cnnAmir(train_opts);
 
     % IMPORTANT NOTE: we randomly undersample when training a model, but then,
