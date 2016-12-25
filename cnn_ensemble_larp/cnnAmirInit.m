a25ee43fd3d5d9c8aff5a7545bea319a7a01f6eb
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2016-12-24 23:01:19 -0500

    update

--- cnn_ensemble_larp/cnnAmirInit.m
+++ cnn_ensemble_larp/cnnAmirInit.m
@@ -37,8 +37,8 @@ function output_opts = cnn_amir_init(input_opts)
           output_opts.train.learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
         case 'mnist-two-class-unbalanced'
           % testing
-          % output_opts.train.learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
-          output_opts.train.learning_rate = [0.05*ones(1,3)];
+          output_opts.train.learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)]; % matconvnet default
+          % output_opts.train.learning_rate = [0.05*ones(1,3)];
         case 'stl-10'
           output_opts.train.learning_rate = [0.5*ones(1,20) 0.05*ones(1,15)  0.1:-0.01:0.06 0.05*ones(1,10)]; % javad-LR
       end
