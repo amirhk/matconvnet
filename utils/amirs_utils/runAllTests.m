5aa00c1e71b2be2495c1fdd167735d7ce976ba16
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-01-04 06:51:08 -0500

    update

--- utils/amirs_utils/runAllTests.m
+++ utils/amirs_utils/runAllTests.m
@@ -53,30 +53,30 @@ function runAllTests(dataset, posneg_balance, gpu);
   single_test_options.gpu = opts.train.gpu;
   single_test_options.backprop_depth = 4;
 
-  % -------------------------------------------------------------------------
-  %                                                               single tree
-  % -------------------------------------------------------------------------
-  % TODO...
+  % % -------------------------------------------------------------------------
+  % %                                                               single tree
+  % % -------------------------------------------------------------------------
+  % % TODO...
 
-  % -------------------------------------------------------------------------
-  %                                                                    forest
-  % -------------------------------------------------------------------------
-  % exp 1
-  single_test_options.boosting_method = 'AdaBoostM1';
-  testForest(single_test_options);
-  % exp 2
-  single_test_options.boosting_method = 'RUSBoost';
-  testForest(single_test_options);
+  % % -------------------------------------------------------------------------
+  % %                                                                    forest
+  % % -------------------------------------------------------------------------
+  % % exp 1
+  % single_test_options.boosting_method = 'AdaBoostM1';
+  % testForest(single_test_options);
+  % % exp 2
+  % single_test_options.boosting_method = 'RUSBoost';
+  % testForest(single_test_options);
 
-  % -------------------------------------------------------------------------
-  %                                                                single cnn
-  % -------------------------------------------------------------------------
-  % exp 1
-  single_test_options.backprop_depth = 4;
-  testSingleNetwork(single_test_options);
-  % exp 2
-  single_test_options.backprop_depth = 13;
-  testSingleNetwork(single_test_options);
+  % % -------------------------------------------------------------------------
+  % %                                                                single cnn
+  % % -------------------------------------------------------------------------
+  % % exp 1
+  % single_test_options.backprop_depth = 4;
+  % testSingleNetwork(single_test_options);
+  % % exp 2
+  % single_test_options.backprop_depth = 13;
+  % testSingleNetwork(single_test_options);
 
   % -------------------------------------------------------------------------
   %                                                              ensemble cnn
@@ -102,23 +102,23 @@ function runAllTests(dataset, posneg_balance, gpu);
   single_test_options.symmetric_weight_updates = true;
   single_test_options.symmetric_loss_updates = false;
   fh.kFoldCNNRusboost(single_test_options);
-  % exp 5
-  single_test_options.backprop_depth = 4;
-  single_test_options.symmetric_weight_updates = false;
-  single_test_options.symmetric_loss_updates = true;
-  fh.kFoldCNNRusboost(single_test_options);
-  % exp 6
-  single_test_options.backprop_depth = 4;
-  single_test_options.symmetric_weight_updates = true;
-  single_test_options.symmetric_loss_updates = true;
-  fh.kFoldCNNRusboost(single_test_options);
-  % exp 7
-  single_test_options.backprop_depth = 13;
-  single_test_options.symmetric_weight_updates = false;
-  single_test_options.symmetric_loss_updates = true;
-  fh.kFoldCNNRusboost(single_test_options);
-  % exp 8
-  single_test_options.backprop_depth = 13;
-  single_test_options.symmetric_weight_updates = true;
-  single_test_options.symmetric_loss_updates = true;
-  fh.kFoldCNNRusboost(single_test_options);
+  % % exp 5
+  % single_test_options.backprop_depth = 4;
+  % single_test_options.symmetric_weight_updates = false;
+  % single_test_options.symmetric_loss_updates = true;
+  % fh.kFoldCNNRusboost(single_test_options);
+  % % exp 6
+  % single_test_options.backprop_depth = 4;
+  % single_test_options.symmetric_weight_updates = true;
+  % single_test_options.symmetric_loss_updates = true;
+  % fh.kFoldCNNRusboost(single_test_options);
+  % % exp 7
+  % single_test_options.backprop_depth = 13;
+  % single_test_options.symmetric_weight_updates = false;
+  % single_test_options.symmetric_loss_updates = true;
+  % fh.kFoldCNNRusboost(single_test_options);
+  % % exp 8
+  % single_test_options.backprop_depth = 13;
+  % single_test_options.symmetric_weight_updates = true;
+  % single_test_options.symmetric_loss_updates = true;
+  % fh.kFoldCNNRusboost(single_test_options);
