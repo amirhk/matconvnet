7b96beaf37abbcbd833c6c995acb36604a37c556
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-01-04 01:04:06 -0500

    update

--- utils/amirs_utils/loadSavedImdb.m
+++ utils/amirs_utils/loadSavedImdb.m
@@ -6,19 +6,21 @@ function imdb = loadSavedImdb(dataset, posneg_balance)
     case 'mnist-two-class'
       switch posneg_balance
         case 'unbalanced'
-          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-mnist-pos9-neg4-unbalanced.mat'));
+          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-mnist-pos9-neg4-unbalanced-30-6000.mat'));
         case 'balanced-low'
           tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-mnist-pos9-neg4-balanced-train-30-30.mat'));
         case 'balanced-high'
-          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-mnist-pos9-neg4-balanced-train-5000-5000.mat'));
+          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-mnist-pos9-neg4-balanced-train-6000-6000.mat'));
       end
     case 'cifar-two-class'
-      case 'unbalanced'
-        tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-cifar-pos5-neg8-unbalanced-25-5000.mat'));
-      case 'balanced-low'
-        tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-cifar-pos5-neg8-balanced-train-25-25.mat'));
-      case 'balanced-high'
-        tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-cifar-pos5-neg8-balanced-train-5000-5000.mat'));
+      switch posneg_balance
+        case 'unbalanced'
+          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-cifar-pos5-neg8-unbalanced-25-5000.mat'));
+        case 'balanced-low'
+          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-cifar-pos5-neg8-balanced-train-25-25.mat'));
+        case 'balanced-high'
+          tmp = load(fullfile(getDevPath(), 'data', 'two_class_imdbs', 'saved-two-class-cifar-pos5-neg8-balanced-train-5000-5000.mat'));
+        end
     case 'prostate'
       fprintf('TODO: implement!')
       % TODO: fixup and test
