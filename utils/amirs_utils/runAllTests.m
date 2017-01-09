ac1bc2dbd52517b21b5524a1510d83e4e09df487
Amir <a6karimi@v1020-wn-36-50.campus-dynamic.uwaterloo.ca>
2017-01-09 09:54:14 -0500

    update

--- utils/amirs_utils/runAllTests.m
+++ utils/amirs_utils/runAllTests.m
@@ -84,43 +84,48 @@ function runAllTests(dataset, posneg_balance, gpus);
   % -------------------------------------------------------------------------
   experiment_options.training_method = 'ensemble-cnn';
   experiment_options.iteration_count = 8;
-  % Exp. 1
-  experiment_options.backprop_depth = 4;
-  experiment_options.symmetric_weight_updates = true;
-  experiment_options.symmetric_loss_updates = true;
-  testKFold(experiment_options);
-  % Exp. 2
-  experiment_options.backprop_depth = 4;
-  experiment_options.symmetric_weight_updates = true;
-  experiment_options.symmetric_loss_updates = false;
-  testKFold(experiment_options);
-  % Exp. 3
-  experiment_options.backprop_depth = 4;
-  experiment_options.symmetric_weight_updates = false;
-  experiment_options.symmetric_loss_updates = true;
-  testKFold(experiment_options);
-  % Exp. 4
-  experiment_options.backprop_depth = 4;
-  experiment_options.symmetric_weight_updates = false;
-  experiment_options.symmetric_loss_updates = false;
-  testKFold(experiment_options);
+  % % Exp. 1
+  % experiment_options.backprop_depth = 4;
+  % experiment_options.symmetric_weight_updates = true;
+  % experiment_options.symmetric_loss_updates = true;
+  % testKFold(experiment_options);
+  % % Exp. 2
+  % experiment_options.backprop_depth = 4;
+  % experiment_options.symmetric_weight_updates = true;
+  % experiment_options.symmetric_loss_updates = false;
+  % testKFold(experiment_options);
+  % % Exp. 3
+  % experiment_options.backprop_depth = 4;
+  % experiment_options.symmetric_weight_updates = false;
+  % experiment_options.symmetric_loss_updates = true;
+  % testKFold(experiment_options);
+  % % Exp. 4
+  % experiment_options.backprop_depth = 4;
+  % experiment_options.symmetric_weight_updates = false;
+  % experiment_options.symmetric_loss_updates = false;
+  % testKFold(experiment_options);
   % Exp. 5
   experiment_options.backprop_depth = 13;
   experiment_options.symmetric_weight_updates = true;
   experiment_options.symmetric_loss_updates = true;
   testKFold(experiment_options);
-  % Exp. 6
-  experiment_options.backprop_depth = 13;
-  experiment_options.symmetric_weight_updates = true;
-  experiment_options.symmetric_loss_updates = false;
-  testKFold(experiment_options);
-  % Exp. 7
-  experiment_options.backprop_depth = 13;
-  experiment_options.symmetric_weight_updates = false;
-  experiment_options.symmetric_loss_updates = true;
-  testKFold(experiment_options);
-  % Exp. 8
-  experiment_options.backprop_depth = 13;
-  experiment_options.symmetric_weight_updates = false;
-  experiment_options.symmetric_loss_updates = false;
+  % % Exp. 6
+  % experiment_options.backprop_depth = 13;
+  % experiment_options.symmetric_weight_updates = true;
+  % experiment_options.symmetric_loss_updates = false;
+  % testKFold(experiment_options);
+  % % Exp. 7
+  % experiment_options.backprop_depth = 13;
+  % experiment_options.symmetric_weight_updates = false;
+  % experiment_options.symmetric_loss_updates = true;
+  % testKFold(experiment_options);
+  % % Exp. 8
+  % experiment_options.backprop_depth = 13;
+  % experiment_options.symmetric_weight_updates = false;
+  % experiment_options.symmetric_loss_updates = false;
+  % testKFold(experiment_options);
+
+  experiment_options.training_method = 'single-cnn';
+  % Exp. 1
+  experiment_options.backprop_depth = 4;
   testKFold(experiment_options);
