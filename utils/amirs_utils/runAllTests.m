39cbb25e74631126d8f568cee9e86ebe82ea0dc7
Amir <a6karimi@v1020-wn-36-50.campus-dynamic.uwaterloo.ca>
2017-01-09 15:52:08 -0500

    update

--- utils/amirs_utils/runAllTests.m
+++ utils/amirs_utils/runAllTests.m
@@ -63,6 +63,7 @@ function runAllTests(dataset, posneg_balance, gpus);
   %                                                              ensemble svm
   % -------------------------------------------------------------------------
   experiment_options.training_method = 'ensemble-svm';
+  experiment_options.iteration_count = 8;
   % Exp. 1
   testKFold(experiment_options);
 
