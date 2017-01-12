d864056bdcb3577aacdeda90808d6265d87738cc
Amir <a6karimi@v1020-wn-36-50.campus-dynamic.uwaterloo.ca>
2017-01-12 13:50:31 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -89,11 +89,11 @@ function runAllTests(dataset, posneg_balance, gpus);
   % -------------------------------------------------------------------------
   %                                                              ensemble svm
   % -------------------------------------------------------------------------
-  experiment_options.training_method = 'ensemble-svm';
-  experiment_options.ensemble_cnn_version = 'v2';
-  experiment_options.iteration_count = 8;
-  % Exp. 1
-  testKFold(experiment_options);
+  % experiment_options.training_method = 'ensemble-svm';
+  % experiment_options.ensemble_cnn_version = 'v1';
+  % experiment_options.iteration_count = 8;
+  % % Exp. 1
+  % testKFold(experiment_options);
 
   % -------------------------------------------------------------------------
   %                                                               single tree
@@ -126,14 +126,14 @@ function runAllTests(dataset, posneg_balance, gpus);
   %                                                              ensemble cnn
   % -------------------------------------------------------------------------
   experiment_options.training_method = 'ensemble-cnn';
-  experiment_options.ensemble_cnn_version = 'v2';
+  experiment_options.ensemble_cnn_version = '1';
   experiment_options.iteration_count = 8;
 
-  % Exp. 1
-  experiment_options.backprop_depth = 4;
-  experiment_options.symmetric_weight_updates = true;
-  experiment_options.symmetric_loss_updates = true;
-  testKFold(experiment_options);
+  % % Exp. 1
+  % experiment_options.backprop_depth = 4;
+  % experiment_options.symmetric_weight_updates = true;
+  % experiment_options.symmetric_loss_updates = true;
+  % testKFold(experiment_options);
   % Exp. 5
   experiment_options.backprop_depth = 13;
   experiment_options.symmetric_weight_updates = true;
