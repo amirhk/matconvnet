cb811ddd6f5f7e8849fa2af6f05ee3456e30fe6a
Amir <a6karimi@v1020-wn-36-50.campus-dynamic.uwaterloo.ca>
2017-01-12 10:25:42 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -125,7 +125,7 @@ function runAllTests(dataset, posneg_balance, gpus);
   %                                                              ensemble cnn
   % -------------------------------------------------------------------------
   experiment_options.training_method = 'ensemble-cnn';
-  experiment_options.ensemble_cnn_version = 'v1';
+  experiment_options.ensemble_cnn_version = 'v2';
   experiment_options.iteration_count = 8;
 
   % Exp. 1
