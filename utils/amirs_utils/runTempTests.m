15d4eb7a942c7734b6d9dbc35e5fcec2693d6a5c
Amir <a6karimi@v1020-wn-36-50.campus-dynamic.uwaterloo.ca>
2017-01-13 09:58:56 -0500

    update

--- utils/amirs_utils/runTempTests.m
+++ utils/amirs_utils/runTempTests.m
@@ -25,9 +25,9 @@ function runTempTests()
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.
 
-  runEnsembleLarpTests('mnist-two-class-9-4',        'balanced-low', 1);
-  runEnsembleLarpTests('svhn-two-class-9-4',         'balanced-low', 1);
-  runEnsembleLarpTests('cifar-two-class-deer-truck', 'balanced-low', 1);
-  runEnsembleLarpTests('mnist-two-class-9-4',        'unbalanced', 1);
-  runEnsembleLarpTests('svhn-two-class-9-4',         'unbalanced', 1);
-  runEnsembleLarpTests('cifar-two-class-deer-truck', 'unbalanced', 1);
+  % runEnsembleLarpTests('mnist-two-class-9-4',        'balanced-low', 2);
+  % runEnsembleLarpTests('svhn-two-class-9-4',         'balanced-low', 2);
+  runEnsembleLarpTests('cifar-two-class-deer-truck', 'balanced-low', 2);
+  % runEnsembleLarpTests('mnist-two-class-9-4',        'unbalanced', 2);
+  runEnsembleLarpTests('svhn-two-class-9-4',         'unbalanced', 2);
+  runEnsembleLarpTests('cifar-two-class-deer-truck', 'unbalanced', 2);
