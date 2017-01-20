24385e79712dc7e8959737136eb4303fb19a6c29
Amir <a6karimi@v1020-wn-36-50.campus-dynamic.uwaterloo.ca>
2017-01-20 14:32:25 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -134,13 +134,13 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   %                                                              ensemble cnn
   % -------------------------------------------------------------------------
   experiment_options.training_method = 'ensemble-cnn';
-  experiment_options.boosting_method = 'rusboost';
+  experiment_options.boosting_method = 'adaboost.m1';
   experiment_options.iteration_count = 8;
 
-  % % Exp. 1
-  % experiment_options.loss_calculation_method = 'default_in_literature';
-  % experiment_options.backprop_depth = 4;
-  % testKFold(experiment_options);
+  % Exp. 1
+  experiment_options.loss_calculation_method = 'default_in_literature';
+  experiment_options.backprop_depth = 4;
+  testKFold(experiment_options);
 
   % % Exp. 2
   % experiment_options.loss_calculation_method = 'default_in_literature';
@@ -152,10 +152,10 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   experiment_options.backprop_depth = 4;
   testKFold(experiment_options);
 
-  % Exp. 4
-  experiment_options.loss_calculation_method = 'class_normalized';
-  experiment_options.backprop_depth = 13;
-  testKFold(experiment_options);
+  % % Exp. 4
+  % experiment_options.loss_calculation_method = 'class_normalized';
+  % experiment_options.backprop_depth = 13;
+  % testKFold(experiment_options);
 
 
 
