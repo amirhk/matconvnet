7025d96640480393fa0f2e8a2afd80f7a5996a78
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-01-31 14:37:10 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -140,21 +140,21 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   % experiment_options.backprop_depth = 13;
   % testKFold(experiment_options);
 
-  % % -------------------------------------------------------------------------
-  % experiment_options.weight_init_sequence = {'quasiRandSobol', 'quasiRandSobol', 'quasiRandSobol'};
-  % % -------------------------------------------------------------------------
+  % -------------------------------------------------------------------------
+  experiment_options.weight_init_sequence = {'quasiRandSobol', 'quasiRandSobol', 'quasiRandSobol'};
+  % -------------------------------------------------------------------------
 
-  % % Exp. 1
-  % experiment_options.backprop_depth = 4;
+  % Exp. 1
+  experiment_options.backprop_depth = 4;
+  testKFold(experiment_options);
+
+  % % Exp. 2
+  % experiment_options.backprop_depth = 7;
   % testKFold(experiment_options);
 
-  % % % Exp. 2
-  % % experiment_options.backprop_depth = 7;
-  % % testKFold(experiment_options);
-
-  % % Exp. 3
-  % experiment_options.backprop_depth = 13;
-  % testKFold(experiment_options);
+  % Exp. 3
+  experiment_options.backprop_depth = 13;
+  testKFold(experiment_options);
 
   % -------------------------------------------------------------------------
   experiment_options.weight_init_sequence = {'quasiRandSobolSkip', 'quasiRandSobolSkip', 'quasiRandSobolSkip'};
