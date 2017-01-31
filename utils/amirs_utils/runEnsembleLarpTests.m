0045bb4903831bbab03d19aff262997ac1ce876d
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-01-31 09:58:30 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -143,9 +143,9 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   experiment_options.iteration_count = 8;
   experiment_options.number_of_samples_per_model = 1000;
 
-  % % -------------------------------------------------------------------------
-  % experiment_options.boosting_method = 'rusboost';
-  % % -------------------------------------------------------------------------
+  % -------------------------------------------------------------------------
+  experiment_options.boosting_method = 'rusboost';
+  % -------------------------------------------------------------------------
 
   % % Exp. 1
   % experiment_options.loss_calculation_method = 'default_in_literature';
@@ -172,39 +172,39 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   % experiment_options.backprop_depth = 7;
   % testKFold(experiment_options);
 
-  % % Exp. 6
-  % experiment_options.loss_calculation_method = 'class_normalized';
-  % experiment_options.backprop_depth = 13;
-  % testKFold(experiment_options);
+  % Exp. 6
+  experiment_options.loss_calculation_method = 'class_normalized';
+  experiment_options.backprop_depth = 13;
+  testKFold(experiment_options);
 
   % -------------------------------------------------------------------------
   experiment_options.boosting_method = 'adaboost.m1';
   % -------------------------------------------------------------------------
 
-  % Exp. 1
-  experiment_options.loss_calculation_method = 'default_in_literature';
-  experiment_options.backprop_depth = 4;
-  testKFold(experiment_options);
+  % % Exp. 1
+  % experiment_options.loss_calculation_method = 'default_in_literature';
+  % experiment_options.backprop_depth = 4;
+  % testKFold(experiment_options);
 
-  % Exp. 2
-  experiment_options.loss_calculation_method = 'default_in_literature';
-  experiment_options.backprop_depth = 7;
-  testKFold(experiment_options);
+  % % Exp. 2
+  % experiment_options.loss_calculation_method = 'default_in_literature';
+  % experiment_options.backprop_depth = 7;
+  % testKFold(experiment_options);
 
-  % Exp. 3
-  experiment_options.loss_calculation_method = 'default_in_literature';
-  experiment_options.backprop_depth = 13;
-  testKFold(experiment_options);
+  % % Exp. 3
+  % experiment_options.loss_calculation_method = 'default_in_literature';
+  % experiment_options.backprop_depth = 13;
+  % testKFold(experiment_options);
 
-  % Exp. 4
-  experiment_options.loss_calculation_method = 'class_normalized';
-  experiment_options.backprop_depth = 4;
-  testKFold(experiment_options);
+  % % Exp. 4
+  % experiment_options.loss_calculation_method = 'class_normalized';
+  % experiment_options.backprop_depth = 4;
+  % testKFold(experiment_options);
 
-  % Exp. 5
-  experiment_options.loss_calculation_method = 'class_normalized';
-  experiment_options.backprop_depth = 7;
-  testKFold(experiment_options);
+  % % Exp. 5
+  % experiment_options.loss_calculation_method = 'class_normalized';
+  % experiment_options.backprop_depth = 7;
+  % testKFold(experiment_options);
 
   % Exp. 6
   experiment_options.loss_calculation_method = 'class_normalized';
