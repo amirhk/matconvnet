648a4d33cddd82f41466ab6594cec2ae89670daa
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-06 16:01:14 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -316,7 +316,7 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   experiment_options.boosting_method = 'adaboost.m1';
 
   % -------------------------------------------------
-  experiment_options.number_of_samples_per_model = 5000;
+  experiment_options.number_of_samples_per_model = 25000;
   % -------------------------------------------------
 
   % Exp. 1
@@ -329,19 +329,19 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   experiment_options.backprop_depth = 13;
   testKFold(experiment_options);
 
-  % -------------------------------------------------
-  experiment_options.number_of_samples_per_model = 10000;
-  % -------------------------------------------------
+  % % -------------------------------------------------
+  % experiment_options.number_of_samples_per_model = 10000;
+  % % -------------------------------------------------
 
-  % Exp. 1
-  experiment_options.loss_calculation_method = 'default_in_literature';
-  experiment_options.backprop_depth = 4;
-  testKFold(experiment_options);
+  % % Exp. 1
+  % experiment_options.loss_calculation_method = 'default_in_literature';
+  % experiment_options.backprop_depth = 4;
+  % testKFold(experiment_options);
 
-  % Exp. 1
-  experiment_options.loss_calculation_method = 'default_in_literature';
-  experiment_options.backprop_depth = 13;
-  testKFold(experiment_options);
+  % % Exp. 1
+  % experiment_options.loss_calculation_method = 'default_in_literature';
+  % experiment_options.backprop_depth = 13;
+  % testKFold(experiment_options);
 
 
 
