a231a9283ac9943a5438b408f0a3e73bce096ca1
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-10 18:54:20 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -230,19 +230,19 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   % testKFold(experiment_options);
 
 
-  % % % -------------------------------------------------
-  % experiment_options.network_arch = 'lenet_no_pool';
-  % % % -------------------------------------------------
+  % % -------------------------------------------------
+  experiment_options.network_arch = 'lenet_no_pool';
+  % % -------------------------------------------------
 
-  % % Exp. 1
-  % experiment_options.backprop_depth = 4;
-  % testKFold(experiment_options);
+  % Exp. 1
+  experiment_options.backprop_depth = 4;
+  testKFold(experiment_options);
 
 
 
-  % -------------------------------------------------
-  experiment_options.network_arch = 'lenet++1';
-  % -------------------------------------------------
+  % % -------------------------------------------------
+  % experiment_options.network_arch = 'lenet++1';
+  % % -------------------------------------------------
 
   % % Exp. 1
   % experiment_options.backprop_depth = 4;
@@ -260,9 +260,9 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   % experiment_options.backprop_depth = 13;
   % testKFold(experiment_options);
 
-  % Exp. 5
-  experiment_options.backprop_depth = 15;
-  testKFold(experiment_options);
+  % % Exp. 5
+  % experiment_options.backprop_depth = 15;
+  % testKFold(experiment_options);
 
 
 
