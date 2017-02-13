68845165e1ce408f24e42f8a0c38e4148ef99ea7
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-13 14:51:25 -0500

    update

--- cnn_amir/getLearningRate.m
+++ cnn_amir/getLearningRate.m
@@ -73,7 +73,9 @@ switch network_arch
     switch dataset
       % multi-class
       case 'mnist'
+        % learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,50)] * 0.01;
         learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,50)] * 0.01;
+
       % multi-class subsampled
       case 'mnist-multi-class-subsampled'
         learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,25)];
