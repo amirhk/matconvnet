d051be79d5f695691a315bcb57ad9165053272a3
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-13 10:15:32 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -425,7 +425,7 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   experiment_options.boosting_method = 'adaboost.m1';
 
   % -------------------------------------------------
-  experiment_options.number_of_samples_per_model = 250000;
+  experiment_options.number_of_samples_per_model = 100000;
   % -------------------------------------------------
 
   % Exp. 1
