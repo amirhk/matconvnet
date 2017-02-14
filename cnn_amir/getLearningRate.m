2d548cba1a5b300d26b91eacac8b31275e4ad5ab
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-14 14:05:08 -0500

    update

--- cnn_amir/getLearningRate.m
+++ cnn_amir/getLearningRate.m
@@ -73,14 +73,14 @@ switch network_arch
     switch dataset
       % multi-class
       case 'mnist'
-        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)]; % ReLU LR
+        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)] / 2; % ReLU LR
         % learning_rate = [0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,50)] * 0.01; % AVR LR
       case 'cifar'
-        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)];
+        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)] / 2;
       case 'mnist-multi-class-subsampled'
-        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)];
+        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)] / 2;
       case 'cifar-multi-class-subsampled'
-        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)];
+        learning_rate = [0.1*ones(1,5) 0.05*ones(1,15) 0.005*ones(1,10) 0.0005*ones(1,45)] / 2;
     end
   case 'lenet_with_larger_fc'
     switch dataset
