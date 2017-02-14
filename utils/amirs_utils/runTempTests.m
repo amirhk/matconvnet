96763e32781ba2e61b47a05cab003ea76e93c1a6
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-13 19:26:33 -0500

    update

--- utils/amirs_utils/runTempTests.m
+++ utils/amirs_utils/runTempTests.m
@@ -51,20 +51,20 @@ function runTempTests()
   % runEnsembleLarpTests('stl-10-multi-class-subsampled', 'balanced-266', 4);
   % runEnsembleLarpTests('stl-10-multi-class-subsampled', 'balanced-500', 4);
 
-
   % runEnsembleLarpTests('mnist', 'whatever', 1);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 1);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-1880', 1);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-707', 1);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-266', 1);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-100', 1);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-38', 1);
 
-  % runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-5000', 2);
-  % runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-1880', 2);
-  % runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-707', 2);
-  % runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-266', 2);
-  % runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-100', 2);
-  % runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-38', 2);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 1);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-1880', 1);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-707', 1);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-266', 1);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-100', 1);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-38', 1);
+
+  runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-5000', 2);
+  runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-1880', 2);
+  runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-707', 2);
+  runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-266', 2);
+  runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-100', 2);
+  runEnsembleLarpTests('cifar-multi-class-subsampled', 'balanced-38', 2);
 
 
