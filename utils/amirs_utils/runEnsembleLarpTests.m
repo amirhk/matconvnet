f1c625b9aec9bb654e22fe90177e72b323230332
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-18 16:16:06 -0500

    update

--- utils/amirs_utils/runEnsembleLarpTests.m
+++ utils/amirs_utils/runEnsembleLarpTests.m
@@ -237,19 +237,19 @@ function runEnsembleLarpTests(dataset, posneg_balance, gpus)
   % experiment_options.backprop_depth = 4;
   % testKFold(experiment_options);
 
-  % -------------------------------------------------
-  experiment_options.network_arch = 'cvv5ap3+fcv1';
-  % -------------------------------------------------
-  % Exp. 1
-  experiment_options.backprop_depth = 4;
-  testKFold(experiment_options);
-
   % % -------------------------------------------------
-  % experiment_options.network_arch = 'cvv5ap5+fcv1';
+  % experiment_options.network_arch = 'cvv5ap3+fcv1';
   % % -------------------------------------------------
   % % Exp. 1
   % experiment_options.backprop_depth = 4;
   % testKFold(experiment_options);
+
+  % -------------------------------------------------
+  experiment_options.network_arch = 'cvv5ap5+fcv1';
+  % -------------------------------------------------
+  % Exp. 1
+  experiment_options.backprop_depth = 4;
+  testKFold(experiment_options);
 
 
 
