a3caccef6b91514e139cc6c0cca84a7a96b9793d
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-02-18 00:31:57 -0500

    update

--- utils/amirs_utils/runTempTests.m
+++ utils/amirs_utils/runTempTests.m
@@ -84,9 +84,9 @@ function runTempTests()
   % runEnsembleLarpTests('svhn-multi-class-subsampled', 'balanced-38', 4);
 
 
-  gpu = 3;
+  gpu = 4;
   % runEnsembleLarpTests('cifar',  'whatever', gpu);
   % runEnsembleLarpTests('mnist',  'whatever', gpu);
-  runEnsembleLarpTests('svhn',   'whatever', gpu);
-  % runEnsembleLarpTests('stl-10', 'whatever', gpu);
+  % runEnsembleLarpTests('svhn',   'whatever', gpu);
+  runEnsembleLarpTests('stl-10', 'whatever', gpu);
 
