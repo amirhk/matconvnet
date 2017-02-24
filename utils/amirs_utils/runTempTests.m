4b8e044c26e97f96cf7e55fa7acfe559353c5c48
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-24 09:37:43 -0500

    update

--- utils/amirs_utils/runTempTests.m
+++ utils/amirs_utils/runTempTests.m
@@ -75,7 +75,7 @@ function runTempTests()
   % runEnsembleLarpTests('cifar-multi-class-subsampled', 'whatever',      'projected-through-larpV3P3+convV0P0+fcV1', gpu);
 
 
-  gpu = 2;
+  gpu = 1;
   % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-38',   'no-projection',                            gpu);
   % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-38',   'projected-through-larpV1P1+convV0P0+fcV1', gpu);
   % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-38',   'projected-through-larpV3P1+convV0P0+fcV1', gpu);
@@ -102,9 +102,9 @@ function runTempTests()
   % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-1880', 'projected-through-larpV3P3+convV0P0+fcV1', gpu);
 
   % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'no-projection',                            gpu);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'projected-through-larpV1P1+convV0P0+fcV1', gpu);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'projected-through-larpV3P1+convV0P0+fcV1', gpu);
-  runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'projected-through-larpV3P3+convV0P0+fcV1', gpu);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'projected-through-larpV1P1+convV0P0+fcV1', gpu);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'projected-through-larpV3P1+convV0P0+fcV1', gpu);
+  % runEnsembleLarpTests('mnist-multi-class-subsampled', 'balanced-5000', 'projected-through-larpV3P3+convV0P0+fcV1', gpu);
 
   runEnsembleLarpTests('mnist-multi-class-subsampled', 'whatever',      'no-projection',                            gpu);
   runEnsembleLarpTests('mnist-multi-class-subsampled', 'whatever',      'projected-through-larpV1P1+convV0P0+fcV1', gpu);
