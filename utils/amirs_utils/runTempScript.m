0317af90f35242c8486823f8698c484ce13e3fbc
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-23 19:09:01 -0500

    added runTempScript.m

---
+++ utils/amirs_utils/runTempScript.m
@@ -0,0 +1,29 @@
+fh = projectionUtils
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-38', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-38', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-38', 'larpV3P3+convV0P0+fcV1', 9);
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-100', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-100', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-100', 'larpV3P3+convV0P0+fcV1', 9);
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-266', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-266', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-266', 'larpV3P3+convV0P0+fcV1', 9);
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-707', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-707', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-707', 'larpV3P3+convV0P0+fcV1', 9);
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV3P3+convV0P0+fcV1', 9);
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV3P3+convV0P0+fcV1', 9);
+
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV1P1+convV0P0+fcV1', 3);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-1880', 'larpV3P3+convV0P0+fcV1', 9);
