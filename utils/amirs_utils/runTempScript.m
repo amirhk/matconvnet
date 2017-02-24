858a7976c417c3ce2a6024a4d050ae43b4ea1dea
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-24 09:51:52 -0500

    update

--- utils/amirs_utils/runTempScript.m
+++ utils/amirs_utils/runTempScript.m
@@ -21,18 +21,9 @@ fh = projectionUtils
 % fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-1880', 'larpV3P3+convV0P0+fcV1', 9);
 
 % fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-5000', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-5000', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-5000', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-5000', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-5000', 'larpV3P3+convV0P0+fcV1', 9);
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar', 'whatever', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar', 'whatever', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar', 'whatever', 'larpV3P3+convV0P0+fcV1', 9);
-
-
-fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-5000', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-5000', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('mnist-multi-class-subsampled', 'balanced-5000', 'larpV3P3+convV0P0+fcV1', 9);
-
-fh.projectAndSaveImdbThroughNetworkArch('mnist', 'whatever', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('mnist', 'whatever', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('mnist', 'whatever', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar', 'whatever', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar', 'whatever', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar', 'whatever', 'larpV3P3+convV0P0+fcV1', 9);
