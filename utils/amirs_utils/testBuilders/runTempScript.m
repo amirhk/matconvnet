5ace880bf6f38aecf104c2c4b637161097f441d7
Amir <a6karimi@v1020-wn-18-123.campus-dynamic.uwaterloo.ca>
2017-02-28 19:35:08 -0500

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -1,28 +1,28 @@
 fh = projectionUtils
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-38', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-38', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-38', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-38', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-38', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-38', 'larpV3P3+convV0P0+fcV1', 9);
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-100', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-100', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-100', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-100', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-100', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-100', 'larpV3P3+convV0P0+fcV1', 9);
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-266', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-266', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-266', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-266', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-266', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-266', 'larpV3P3+convV0P0+fcV1', 9);
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-707', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-707', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-707', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-707', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-707', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-707', 'larpV3P3+convV0P0+fcV1', 9);
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-1880', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-1880', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-1880', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-1880', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-1880', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-1880', 'larpV3P3+convV0P0+fcV1', 9);
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-5000', 'larpV1P1+convV0P0+fcV1', 3);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-5000', 'larpV3P1+convV0P0+fcV1', 7);
-fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-5000', 'larpV3P3+convV0P0+fcV1', 9);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-5000', 'larpV1P1+convV0P0+fcV1', 3);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-5000', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white-multi-class-subsampled', 'balanced-5000', 'larpV3P3+convV0P0+fcV1', 9);
 
 fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white', 'whatever', 'larpV1P1+convV0P0+fcV1', 3);
 fh.projectAndSaveImdbThroughNetworkArch('cifar-no-white', 'whatever', 'larpV3P1+convV0P0+fcV1', 7);
