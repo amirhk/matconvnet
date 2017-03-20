29ffe5bd97e645ebc25cad46a27dfc781d8da6ad
Amir <a6karimi@v1020-wn-23-8.campus-dynamic.uwaterloo.ca>
2017-03-19 22:07:09 -0400

    update

--- cnn_amir/testCnn.m
+++ cnn_amir/testCnn.m
@@ -260,4 +260,4 @@ function [images, labels] = getSimpleNNBatch(imdb, batch)
 % -------------------------------------------------------------------------
   images = imdb.images.data(:,:,:,batch);
   labels = imdb.images.labels(1,batch);
-  % if rand > 0.5, images=fliplr(images); end
+  if rand > 0.5, images=fliplr(images); end
