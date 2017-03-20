42a42f54b96da9938aea6f2e3ba0593d67e03dc2
Amir <a6karimi@v1020-wn-23-8.campus-dynamic.uwaterloo.ca>
2017-03-19 22:36:29 -0400

    [CHECKPOINT] I may have found the bug in saving forward projections... fliplr should not be done in getBatch

--- utils/amirs_utils/getPredictionsFromModelOnImdb.m
+++ utils/amirs_utils/getPredictionsFromModelOnImdb.m
@@ -252,7 +252,7 @@ function [images, labels] = getSimpleNNBatch(imdb, batch)
 % -------------------------------------------------------------------------
   images = imdb.images.data(:,:,:,batch);
   labels = imdb.images.labels(1,batch);
-  if rand > 0.5, images=fliplr(images); end
+  % if rand > 0.5, images=fliplr(images); end
 
 % -------------------------------------------------------------------------
 function all_predictions = getAllPredictionsFromTopPredictions(top_predictions, imdb)
