130d30f909f1b3f7d41cc9721219f9a0a2f6130e
Amir <a6karimi@v1020-wn-23-8.campus-dynamic.uwaterloo.ca>
2017-03-19 21:12:44 -0400

    update

--- cnn_amir/cnnInit.m
+++ cnn_amir/cnnInit.m
@@ -117,6 +117,41 @@ function network_opts = cnnInit(input_opts)
       net.layers{end+1} = fh.softmaxlossLayer();
       % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
+    case 'larpV3P3+convV0P0+fcV1'
+      layer_number = 1;
+      net.layers{end+1} = fh.convLayer(opts.dataset, opts.network_arch, layer_number, 5, 3, 32, 1/100, 2, 'compRand', 'gen');
+      net.layers{end+1} = fh.poolingLayerLeNetMax(layer_number);
+      net.layers{end+1} = fh.reluLayer(layer_number);
+
+      layer_number = layer_number + 3;
+      net.layers{end+1} = fh.convLayer(opts.dataset, opts.network_arch, layer_number, 5, 32, 32, 5/100, 2, 'compRand', 'gen');
+      net.layers{end+1} = fh.reluLayer(layer_number);
+      net.layers{end+1} = fh.poolingLayerLeNetAvg(layer_number);
+
+      layer_number = layer_number + 3;
+      net.layers{end+1} = fh.convLayer(opts.dataset, opts.network_arch, layer_number, 5, 32, 64, 5/100, 2, 'compRand', 'gen');
+      net.layers{end+1} = fh.reluLayer(layer_number);
+      net.layers{end+1} = fh.poolingLayerLeNetAvg(layer_number);
+
+      % FULLY CONNECTED
+      layer_number = layer_number + 3;
+      net.layers{end+1} = fh.convLayer(opts.dataset, opts.network_arch, layer_number, 4, 64, 64, 5/100, 0, 'compRand', 'gen');
+      net.layers{end+1} = fh.reluLayer(layer_number);
+
+      layer_number = layer_number + 2;
+      number_of_output_nodes = getNumberOfOutputNodes(opts.dataset);
+      net.layers{end+1} = fh.convLayer(opts.dataset, opts.network_arch, layer_number, 1, 64, number_of_output_nodes, 5/100, 0, 'compRand', 'gen');
+
+      % LOSS LAYER
+      net.layers{end+1} = fh.softmaxlossLayer();
+      % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+
+
+
+
+
     case 'larpV0P0+convV0P0+fcV2'
       % -----------------------------------------------------------------------
       %                                                      FC LENET WITH CONV
--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -21,7 +21,7 @@ fh = projectionUtils;
 % fh.projectAndSaveImdbThroughNetworkArch('mnist',                        'whatever',    'larpV3P3ST', 9);
 
 
-fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-38', 'larpV3P1+convV0P0+fcV1', 7);
+% fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-38', 'larpV3P1+convV0P0+fcV1', 7);
 % fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-38', 'larpV1P0ST', 2);
 % fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-38', 'larpV1P1ST', 3);
 % fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-38', 'larpV3P0ST', 6);
@@ -29,6 +29,7 @@ fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balance
 % fh.projectAndSaveImdbThroughNetworkArch('cifar-multi-class-subsampled', 'balanced-38', 'larpV3P3ST', 9);
 
 % fh.projectAndSaveImdbThroughNetworkArch('cifar',                          'whatever', 'larpV3P1+convV0P0+fcV1', 7);
+fh.projectAndSaveImdbThroughNetworkArch('cifar',                          'whatever', 'larpV3P3+convV0P0+fcV1', 9);
 % fh.projectAndSaveImdbThroughNetworkArch('cifar',                        'whatever',    'larpV1P0ST', 2);
 % fh.projectAndSaveImdbThroughNetworkArch('cifar',                        'whatever',    'larpV1P1ST', 3);
 % fh.projectAndSaveImdbThroughNetworkArch('cifar',                        'whatever',    'larpV3P0ST', 6);
