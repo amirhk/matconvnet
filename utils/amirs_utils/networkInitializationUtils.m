399d9de22b606db7bb5f9a34c2f81e4a3964ca2f
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-03-24 22:43:36 -0400

    update

--- utils/amirs_utils/networkInitializationUtils.m
+++ utils/amirs_utils/networkInitializationUtils.m
@@ -222,7 +222,7 @@ function structuredLayer = convLayer(dataset, network_arch, layer_number, k, m, 
                 case 5
                   mu = mu + reshape(get5x5SobelHoriz(), 1, k * k); % mu must be row vector
               end
-              mu = mu / 3;
+              mu = mu / 30;
               single_generated_kernel = mvnrnd(mu, sigma,1);
               tmp_kernels(:,:,i,j) = reshape(single_generated_kernel, k, k);
               % random_std = rand() * 3;
