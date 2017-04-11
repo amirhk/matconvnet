8a76b81ebf62f9f6c4d3c037a5a41c1ef1b29e87
Amir <a6karimi@v1020-wn-78-222.campus-dynamic.uwaterloo.ca>
2017-04-11 19:42:11 -0400

    update

--- utils/amirs_utils/getPerformanceSummary.m
+++ utils/amirs_utils/getPerformanceSummary.m
@@ -25,6 +25,7 @@ function [accuracy, sensitivity, specificity] = getPerformanceSummary(model_obje
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 % POSSIBILITY OF SUCH DAMAGE.
 
+  keyboard
   if isTwoClassImdb(dataset)
     fhGetAccSensSpec = @getAccSensSpec;
   else
