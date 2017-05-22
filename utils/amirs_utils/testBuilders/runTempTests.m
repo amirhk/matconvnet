bec7f68b4b957f5bbd72aaa59c3d3d2cee3b08a5
Amir <a6karimi@v1020-wn-222-180.campus-dynamic.uwaterloo.ca>
2017-05-22 14:57:41 -0400

    update

--- utils/amirs_utils/testBuilders/runTempTests.m
+++ utils/amirs_utils/testBuilders/runTempTests.m
@@ -218,8 +218,8 @@ function runTempTests()
 
 
 
-  dataset_name = 'cifar-multi-class-subsampled';    runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
-  % dataset_name = 'mnist-multi-class-subsampled';    runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
+  % dataset_name = 'cifar-multi-class-subsampled';    runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
+  dataset_name = 'mnist-multi-class-subsampled';    runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
   % dataset_name = 'svhn-multi-class-subsampled';   runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
   % dataset_name = 'stl-10-multi-class-subsampled'; runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
 
