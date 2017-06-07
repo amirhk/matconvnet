464b7f41acfd98f22f26efcdd458762a24eef7cf
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-06-06 20:42:04 -0400

    update

--- utils/amirs_utils/testBuilders/runTempTests.m
+++ utils/amirs_utils/testBuilders/runTempTests.m
@@ -34,7 +34,7 @@ function runTempTests()
   % -------------------------------------------------------------------------
   %                                                                opts.train
   % -------------------------------------------------------------------------
-  opts.train.gpus = 1;
+  opts.train.gpus = 2;
 
 
   % -------------------------------------------------------------------------
@@ -116,27 +116,27 @@ function runTempTests()
 
 
 
-  larp_network_arch = 'larpV1P0RL0';
-  non_larp_network_arch = 'convV0P0RL0+fcV1';
-  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
-
-
-
-  larp_network_arch = 'larpV1P0RL1';
-  non_larp_network_arch = 'convV0P0RL0+fcV1';
-  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
-
-
-
-  % larp_network_arch = 'larpV3P0RL0';
+  % larp_network_arch = 'larpV1P0RL0';
   % non_larp_network_arch = 'convV0P0RL0+fcV1';
   % dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
 
 
 
-  % larp_network_arch = 'larpV3P0RL3';
+  % larp_network_arch = 'larpV1P0RL1';
   % non_larp_network_arch = 'convV0P0RL0+fcV1';
   % dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
+
+
+
+  larp_network_arch = 'larpV3P0RL0';
+  non_larp_network_arch = 'convV0P0RL0+fcV1';
+  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
+
+
+
+  larp_network_arch = 'larpV3P0RL3';
+  non_larp_network_arch = 'convV0P0RL0+fcV1';
+  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
 
 
 
@@ -163,27 +163,27 @@ function runTempTests()
 
 
 
-  larp_network_arch = 'larpV0P0RL0';
-  non_larp_network_arch = 'convV1P0RL0+fcV1';
-  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
-
-
-
-  larp_network_arch = 'larpV0P0RL0';
-  non_larp_network_arch = 'convV1P0RL1+fcV1';
-  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
-
-
-
   % larp_network_arch = 'larpV0P0RL0';
-  % non_larp_network_arch = 'convV3P0RL0+fcV1';
+  % non_larp_network_arch = 'convV1P0RL0+fcV1';
   % dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
 
 
 
   % larp_network_arch = 'larpV0P0RL0';
-  % non_larp_network_arch = 'convV3P0RL3+fcV1';
+  % non_larp_network_arch = 'convV1P0RL1+fcV1';
   % dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
+
+
+
+  larp_network_arch = 'larpV0P0RL0';
+  non_larp_network_arch = 'convV3P0RL0+fcV1';
+  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
+
+
+
+  larp_network_arch = 'larpV0P0RL0';
+  non_larp_network_arch = 'convV3P0RL3+fcV1';
+  dataset_name = 'cifar-multi-class-subsampled';  runLarpTests(opts.paths.experiment_dir, dataset_name, balance_name, larp_network_arch, non_larp_network_arch, larp_weight_init_type, opts.train.gpus);
 
 
 
