8b5344c5a5f544ebe02053f0e57dda93f89c5338
Amir <a6karimi@v1020-wn-208-42.campus-dynamic.uwaterloo.ca>
2017-06-15 12:21:45 -0400

    update

--- utils/amirs_utils/testBuilders/setupExperimentsUsingProjectedImbds.m
+++ utils/amirs_utils/testBuilders/setupExperimentsUsingProjectedImbds.m
@@ -72,13 +72,13 @@ function [original_imdb, experiments] = setupExperimentsUsingProjectedImbds(data
   % experiments{end}.title = larp_network_arch;
   % afprintf(sprintf('[INFO] done!\n'));
 
-  afprintf(sprintf('[INFO] Loading projected imdb...\n'));
-  larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
-  larp_network_arch = 'larpV1P0RL1';
-  projected_imdb = getRandomlyProjectedImdb(original_imdb, dataset, larp_weight_init_type, larp_network_arch, -1);
-  experiments{end+1}.imdb = projected_imdb;
-  experiments{end}.title = larp_network_arch;
-  afprintf(sprintf('[INFO] done!\n'));
+  % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
+  % larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
+  % larp_network_arch = 'larpV1P0RL1';
+  % projected_imdb = getRandomlyProjectedImdb(original_imdb, dataset, larp_weight_init_type, larp_network_arch, -1);
+  % experiments{end+1}.imdb = projected_imdb;
+  % experiments{end}.title = larp_network_arch;
+  % afprintf(sprintf('[INFO] done!\n'));
 
   % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
   % larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
@@ -88,13 +88,13 @@ function [original_imdb, experiments] = setupExperimentsUsingProjectedImbds(data
   % experiments{end}.title = larp_network_arch;
   % afprintf(sprintf('[INFO] done!\n'));
 
-  afprintf(sprintf('[INFO] Loading projected imdb...\n'));
-  larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
-  larp_network_arch = 'larpV3P0RL3';
-  projected_imdb = getRandomlyProjectedImdb(original_imdb, dataset, larp_weight_init_type, larp_network_arch, -1);
-  experiments{end+1}.imdb = projected_imdb;
-  experiments{end}.title = larp_network_arch;
-  afprintf(sprintf('[INFO] done!\n'));
+  % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
+  % larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
+  % larp_network_arch = 'larpV3P0RL3';
+  % projected_imdb = getRandomlyProjectedImdb(original_imdb, dataset, larp_weight_init_type, larp_network_arch, -1);
+  % experiments{end+1}.imdb = projected_imdb;
+  % experiments{end}.title = larp_network_arch;
+  % afprintf(sprintf('[INFO] done!\n'));
 
   % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
   % larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
@@ -104,13 +104,13 @@ function [original_imdb, experiments] = setupExperimentsUsingProjectedImbds(data
   % experiments{end}.title = larp_network_arch;
   % afprintf(sprintf('[INFO] done!\n'));
 
-  afprintf(sprintf('[INFO] Loading projected imdb...\n'));
-  larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
-  larp_network_arch = 'larpV3P3RL3';
-  projected_imdb = getRandomlyProjectedImdb(original_imdb, dataset, larp_weight_init_type, larp_network_arch, -1);
-  experiments{end+1}.imdb = projected_imdb;
-  experiments{end}.title = sprintf('%s (LeNet)', larp_network_arch);
-  afprintf(sprintf('[INFO] done!\n'));
+  % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
+  % larp_weight_init_type = 'gaussian-IdentityCovariance-MuDivide-1-SigmaDivide-1';
+  % larp_network_arch = 'larpV3P3RL3';
+  % projected_imdb = getRandomlyProjectedImdb(original_imdb, dataset, larp_weight_init_type, larp_network_arch, -1);
+  % experiments{end+1}.imdb = projected_imdb;
+  % experiments{end}.title = sprintf('%s (LeNet)', larp_network_arch);
+  % afprintf(sprintf('[INFO] done!\n'));
 
 
   % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
@@ -258,12 +258,12 @@ function [original_imdb, experiments] = setupExperimentsUsingProjectedImbds(data
   % experiments{end}.title = 'Trained larpV0P0RL0+convV1P0RL0 - trained on all';
   % afprintf(sprintf('[INFO] done!\n'));
 
-  % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
-  % projection_net = loadTrainedNet('larpV0P0RL0+convV1P0RL1', 'cifar', 'balanced-all');
-  % projected_imdb = fh_projection_utils.projectImdbThroughNetwork(original_imdb, projection_net, 2);
-  % experiments{end+1}.imdb = projected_imdb;
-  % experiments{end}.title = 'Trained larpV0P0RL0+convV1P0RL1 - trained on all';
-  % afprintf(sprintf('[INFO] done!\n'));
+  afprintf(sprintf('[INFO] Loading projected imdb...\n'));
+  projection_net = loadTrainedNet('larpV0P0RL0+convV1P0RL1', 'cifar', 'balanced-all');
+  projected_imdb = fh_projection_utils.projectImdbThroughNetwork(original_imdb, projection_net, 2);
+  experiments{end+1}.imdb = projected_imdb;
+  experiments{end}.title = 'Trained larpV0P0RL0+convV1P0RL1 - trained on all';
+  afprintf(sprintf('[INFO] done!\n'));
 
   % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
   % projection_net = loadTrainedNet('larpV0P0RL0+convV3P0RL0', 'cifar', 'balanced-all');
@@ -272,12 +272,12 @@ function [original_imdb, experiments] = setupExperimentsUsingProjectedImbds(data
   % experiments{end}.title = 'Trained larpV0P0RL0+convV3P0RL0 - trained on all';
   % afprintf(sprintf('[INFO] done!\n'));
 
-  % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
-  % projection_net = loadTrainedNet('larpV0P0RL0+convV3P0RL3', 'cifar', 'balanced-all');
-  % projected_imdb = fh_projection_utils.projectImdbThroughNetwork(original_imdb, projection_net, 6);
-  % experiments{end+1}.imdb = projected_imdb;
-  % experiments{end}.title = 'Trained larpV0P0RL0+convV3P0RL3 - trained on all';
-  % afprintf(sprintf('[INFO] done!\n'));
+  afprintf(sprintf('[INFO] Loading projected imdb...\n'));
+  projection_net = loadTrainedNet('larpV0P0RL0+convV3P0RL3', 'cifar', 'balanced-all');
+  projected_imdb = fh_projection_utils.projectImdbThroughNetwork(original_imdb, projection_net, 6);
+  experiments{end+1}.imdb = projected_imdb;
+  experiments{end}.title = 'Trained larpV0P0RL0+convV3P0RL3 - trained on all';
+  afprintf(sprintf('[INFO] done!\n'));
 
   % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
   % projection_net = loadTrainedNet('larpV0P0RL0+convV3P3RL0', 'cifar', 'balanced-all');
@@ -286,12 +286,12 @@ function [original_imdb, experiments] = setupExperimentsUsingProjectedImbds(data
   % experiments{end}.title = 'Trained larpV0P0RL0+convV3P3RL0 - trained on all';
   % afprintf(sprintf('[INFO] done!\n'));
 
-  % afprintf(sprintf('[INFO] Loading projected imdb...\n'));
-  % projection_net = loadTrainedNet('larpV0P0RL0+convV3P3RL3', 'cifar', 'balanced-all');
-  % projected_imdb = fh_projection_utils.projectImdbThroughNetwork(original_imdb, projection_net, 9);
-  % experiments{end+1}.imdb = projected_imdb;
-  % experiments{end}.title = 'Trained larpV0P0RL0+convV3P3RL3 (LeNet) - trained on all';
-  % afprintf(sprintf('[INFO] done!\n'));
+  afprintf(sprintf('[INFO] Loading projected imdb...\n'));
+  projection_net = loadTrainedNet('larpV0P0RL0+convV3P3RL3', 'cifar', 'balanced-all');
+  projected_imdb = fh_projection_utils.projectImdbThroughNetwork(original_imdb, projection_net, 9);
+  experiments{end+1}.imdb = projected_imdb;
+  experiments{end}.title = 'Trained larpV0P0RL0+convV3P3RL3 (LeNet) - trained on all';
+  afprintf(sprintf('[INFO] done!\n'));
 
 
   % -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
