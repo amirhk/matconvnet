b6cdd4d0acae66d54844267ae53890a422ab27df
Amir <a6karimi@v1020-wn-208-42.campus-dynamic.uwaterloo.ca>
2017-06-15 14:32:43 -0400

    update

--- utils/amirs_utils/tempScriptRunTsne.m
+++ utils/amirs_utils/tempScriptRunTsne.m
@@ -60,7 +60,7 @@ function tempScriptRunTsne()
     j =  1;
     % for perplexity = [2, 5, 30, 50, 100]
     for perplexity = [30]
-      subplot(1, 5, j);
+      subplot(1, numel(perplexity), j);
       hold on
       title(sprintf('perplexity: %d', perplexity));
       % Run t−SNE
