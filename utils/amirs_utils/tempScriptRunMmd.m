a6add9221d105ca03cfcf639738d942414314d4c
Amir <a6karimi@v1020-wn-208-42.campus-dynamic.uwaterloo.ca>
2017-06-17 15:54:33 -0400

    update

--- utils/amirs_utils/tempScriptRunMmd.m
+++ utils/amirs_utils/tempScriptRunMmd.m
@@ -34,9 +34,9 @@ function tempScriptRunMmd()
   % posneg_balance = 'balanced-266';
   % dataset = 'cifar-two-class-deer-truck';
   % dataset = 'gaussian-5D-160-train-40-test';
-  dataset = 'gaussian-10D-160-train-40-test';
-  % dataset = 'gaussian-25D-160-train-40-test';
-  % dataset = 'gaussian-100D-160-train-40-test';
+  % dataset = 'gaussian-10D-160-train-40-test';
+  dataset = 'gaussian-25D-160-train-40-test';
+  % dataset = 'gaussian-50D-160-train-40-test';
   posneg_balance = 'balanced-38';
 
   [~, experiments] = setupExperimentsUsingProjectedImbds(dataset, posneg_balance, 1);
