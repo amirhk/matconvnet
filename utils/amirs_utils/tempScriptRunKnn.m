c55a35112249eca69cd16e4569b203101771cf2e
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-06-18 01:50:40 -0400

    update

--- utils/amirs_utils/tempScriptRunKnn.m
+++ utils/amirs_utils/tempScriptRunKnn.m
@@ -50,7 +50,7 @@ function tempScriptRunKNN()
 
   dataset = 'gaussian-5D-160-train-40-test-0.1-var';
   dataset = 'gaussian-5D-160-train-40-test-1.0-var';
-  % dataset = 'gaussian-5D-160-train-40-test-10.0-var';
+  dataset = 'gaussian-5D-160-train-40-test-10.0-var';
 
   % dataset = 'gaussian-5D-400-train-100-test-0.1-var';
   % dataset = 'gaussian-5D-400-train-100-test-1.0-var';
