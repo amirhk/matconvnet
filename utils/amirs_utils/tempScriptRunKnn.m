b2b3a00e19ffa2205f0946009e4e44d65cb44bbd
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-06-18 01:50:28 -0400

    update

--- utils/amirs_utils/tempScriptRunKnn.m
+++ utils/amirs_utils/tempScriptRunKnn.m
@@ -49,7 +49,7 @@ function tempScriptRunKNN()
   % generate more of the 0.1 variances and comapre
 
   dataset = 'gaussian-5D-160-train-40-test-0.1-var';
-  % dataset = 'gaussian-5D-160-train-40-test-1.0-var';
+  dataset = 'gaussian-5D-160-train-40-test-1.0-var';
   % dataset = 'gaussian-5D-160-train-40-test-10.0-var';
 
   % dataset = 'gaussian-5D-400-train-100-test-0.1-var';
