d493b497208f76d9d4fa5e94aebb0769984ff8c8
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-06-18 03:10:10 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -1,26 +1,26 @@
-tempScriptRunKNN('gaussian-5D-160-train-40-test-0.1-var')
-tempScriptRunKNN('gaussian-5D-160-train-40-test-1.0-var')
-tempScriptRunKNN('gaussian-5D-160-train-40-test-10.0-var')
+tempScriptRunKnn('gaussian-5D-160-train-40-test-0.1-var');
+tempScriptRunKnn('gaussian-5D-160-train-40-test-1.0-var');
+tempScriptRunKnn('gaussian-5D-160-train-40-test-10.0-var');
 
-tempScriptRunKNN('gaussian-5D-400-train-100-test-0.1-var')
-tempScriptRunKNN('gaussian-5D-400-train-100-test-1.0-var')
-tempScriptRunKNN('gaussian-5D-400-train-100-test-10.0-var')
+tempScriptRunKnn('gaussian-5D-400-train-100-test-0.1-var');
+tempScriptRunKnn('gaussian-5D-400-train-100-test-1.0-var');
+tempScriptRunKnn('gaussian-5D-400-train-100-test-10.0-var');
 
-tempScriptRunKNN('gaussian-5D-800-train-200-test-0.1-var')
-tempScriptRunKNN('gaussian-5D-800-train-200-test-1.0-var')
-tempScriptRunKNN('gaussian-5D-800-train-200-test-10.0-var')
+tempScriptRunKnn('gaussian-5D-800-train-200-test-0.1-var');
+tempScriptRunKnn('gaussian-5D-800-train-200-test-1.0-var');
+tempScriptRunKnn('gaussian-5D-800-train-200-test-10.0-var');
 
 
 
-tempScriptRunKNN('gaussian-50D-160-train-40-test-0.1-var')
-tempScriptRunKNN('gaussian-50D-160-train-40-test-1.0-var')
-tempScriptRunKNN('gaussian-50D-160-train-40-test-10.0-var')
+tempScriptRunKnn('gaussian-50D-160-train-40-test-0.1-var');
+tempScriptRunKnn('gaussian-50D-160-train-40-test-1.0-var');
+tempScriptRunKnn('gaussian-50D-160-train-40-test-10.0-var');
 
-tempScriptRunKNN('gaussian-50D-400-train-100-test-0.1-var')
-tempScriptRunKNN('gaussian-50D-400-train-100-test-1.0-var')
-tempScriptRunKNN('gaussian-50D-400-train-100-test-10.0-var')
+tempScriptRunKnn('gaussian-50D-400-train-100-test-0.1-var');
+tempScriptRunKnn('gaussian-50D-400-train-100-test-1.0-var');
+tempScriptRunKnn('gaussian-50D-400-train-100-test-10.0-var');
 
-tempScriptRunKNN('gaussian-50D-800-train-200-test-0.1-var')
-tempScriptRunKNN('gaussian-50D-800-train-200-test-1.0-var')
-tempScriptRunKNN('gaussian-50D-800-train-200-test-10.0-var')
+tempScriptRunKnn('gaussian-50D-800-train-200-test-0.1-var');
+tempScriptRunKnn('gaussian-50D-800-train-200-test-1.0-var');
+tempScriptRunKnn('gaussian-50D-800-train-200-test-10.0-var');
 
