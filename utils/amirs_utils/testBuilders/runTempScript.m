e4d1fc2cdeca8d5984af52d37ea2f04a5d6408a9
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-06-18 03:09:02 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -24,27 +24,3 @@ tempScriptRunKNN('gaussian-50D-800-train-200-test-0.1-var')
 tempScriptRunKNN('gaussian-50D-800-train-200-test-1.0-var')
 tempScriptRunKNN('gaussian-50D-800-train-200-test-10.0-var')
 
-
-
-
-constructSyntheticGaussianImdb(100, 5, 0.1);
-constructSyntheticGaussianImdb(100, 5, 0.1);
-constructSyntheticGaussianImdb(100, 5, 1);
-constructSyntheticGaussianImdb(100, 5, 10);
-constructSyntheticGaussianImdb(250, 5, 0.1);
-constructSyntheticGaussianImdb(250, 5, 1);
-constructSyntheticGaussianImdb(250, 5, 10);
-constructSyntheticGaussianImdb(500, 5, 0.1);
-constructSyntheticGaussianImdb(500, 5, 1);
-constructSyntheticGaussianImdb(500, 5, 10);
-
-constructSyntheticGaussianImdb(100, 50, 0.1);
-constructSyntheticGaussianImdb(100, 50, 0.1);
-constructSyntheticGaussianImdb(100, 50, 1);
-constructSyntheticGaussianImdb(100, 50, 10);
-constructSyntheticGaussianImdb(250, 50, 0.1);
-constructSyntheticGaussianImdb(250, 50, 1);
-constructSyntheticGaussianImdb(250, 50, 10);
-constructSyntheticGaussianImdb(500, 50, 0.1);
-constructSyntheticGaussianImdb(500, 50, 1);
-constructSyntheticGaussianImdb(500, 50, 10);
