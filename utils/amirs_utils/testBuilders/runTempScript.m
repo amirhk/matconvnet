d41e4ac9d76fbb322272dbf38a61c62b98aca1d0
Amir <a6karimi@v1020-wn-125-104.campus-dynamic.uwaterloo.ca>
2017-06-20 15:23:03 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -30,8 +30,14 @@
 % tempScriptRunKnn('saved-multi-class-gaussian-50D-mean-9-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
 
-tempScriptRunKnn('uci-ion', 'balanced-38', 1);
-tempScriptRunKnn('uci-spam', 'balanced-38', 1);
+% tempScriptRunKnn('uci-ion', 'balanced-38', 1);
+tempScriptRunTsne('uci-ion', 'balanced-38', 1);
+% calculateDistances('uci-ion', 'balanced-38', 1);
+
+% tempScriptRunKnn('uci-spam', 'balanced-38', 1);
+tempScriptRunTsne('uci-spam', 'balanced-38', 1);
+% calculateDistances('uci-spam', 'balanced-38', 1);
+
 
 
 
