9cbceb3341c5b9195f47dea01eb4f1f1513c2e9e
Amir <a6karimi@v1020-wn-125-104.campus-dynamic.uwaterloo.ca>
2017-06-20 14:24:38 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -30,6 +30,9 @@
 % tempScriptRunKnn('saved-multi-class-gaussian-50D-mean-9-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
 
+tempScriptRunKnn('uci-spam', 'balanced-38', 0);
+
+
 
 
 
@@ -47,15 +50,15 @@
 % calculateDistances('saved-multi-class-gaussian-5D-mean-1-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
 % calculateDistances('saved-multi-class-gaussian-5D-mean-1-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
-calculateDistances('saved-multi-class-gaussian-5D-mean-9-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
-calculateDistances('saved-multi-class-gaussian-5D-mean-9-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
-calculateDistances('saved-multi-class-gaussian-5D-mean-9-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-5D-mean-9-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-5D-mean-9-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-5D-mean-9-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
-calculateDistances('saved-multi-class-gaussian-50D-mean-1-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
-calculateDistances('saved-multi-class-gaussian-50D-mean-1-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
-calculateDistances('saved-multi-class-gaussian-50D-mean-1-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-50D-mean-1-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-50D-mean-1-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-50D-mean-1-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
-calculateDistances('saved-multi-class-gaussian-50D-mean-9-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
-calculateDistances('saved-multi-class-gaussian-50D-mean-9-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
-calculateDistances('saved-multi-class-gaussian-50D-mean-9-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-50D-mean-9-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-50D-mean-9-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
+% calculateDistances('saved-multi-class-gaussian-50D-mean-9-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
