ed665329b0862f19edc08fc0e5ff67c50cd88d8a
Amir <a6karimi@v1020-wn-125-104.campus-dynamic.uwaterloo.ca>
2017-06-26 11:29:03 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -37,7 +37,7 @@ functionHandle('saved-multi-class-gaussian-2D-mean-3-var-3-train-balance-500-tes
 
 % functionHandle('saved-multi-class-gaussian-50D-mean-1-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
 % functionHandle('saved-multi-class-gaussian-50D-mean-1-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
-% functionHandle('saved-multi-class-gaussian-50D-mean-1-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
+functionHandle('saved-multi-class-gaussian-50D-mean-1-var-10-train-balance-500-test-balance-500', 'balanced-38', 1);
 
 % functionHandle('saved-multi-class-gaussian-50D-mean-9-var-0-train-balance-500-test-balance-500', 'balanced-38', 1);
 % functionHandle('saved-multi-class-gaussian-50D-mean-9-var-1-train-balance-500-test-balance-500', 'balanced-38', 1);
@@ -45,7 +45,7 @@ functionHandle('saved-multi-class-gaussian-2D-mean-3-var-3-train-balance-500-tes
 
 % functionHandle('saved-multi-class-gaussian-50D-mean-1-var-10-train-balance-2500-test-balance-2500', 'balanced-38', 1);
 
-% functionHandle('uci-ion', 'balanced-38', 1);
-% functionHandle('uci-spam', 'balanced-38', 1);
+functionHandle('uci-ion', 'balanced-38', 1);
+functionHandle('uci-spam', 'balanced-38', 1);
 
 % functionHandle('cifar-multi-class-subsampled', 'balanced-1880', 1);
