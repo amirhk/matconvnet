2bad9912ec29042d167012bda54b5d27d4cb875c
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-07-12 23:13:00 -0400

    update

--- utils/amirs_utils/tempScriptReproDasgupta.m
+++ utils/amirs_utils/tempScriptReproDasgupta.m
@@ -30,8 +30,8 @@ function tempScriptReproDasgupta()
   number_of_samples_list = 25:25:500;
 
   % test_type = 'vary_original_dim';
-  % test_type = 'vary_projected_dim';
-  test_type = 'vary_number_of_samples';
+  test_type = 'vary_projected_dim';
+  % test_type = 'vary_number_of_samples';
 
   % metric = 'c separation';
   % metric = 'eccentricity';
