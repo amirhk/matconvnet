4540c2528e319ec4da25c66829d0957913b2b5ac
Amir <a6karimi@v1020-wn-12-227.campus-dynamic.uwaterloo.ca>
2017-07-13 16:41:49 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -17,10 +17,10 @@
 
 
 
-tempScriptReproDasgupta('measure-c-separation', 'rp_1_relu_0');
-tempScriptReproDasgupta('measure-1-knn-perf', 'rp_1_relu_0');
-% tempScriptReproDasgupta('measure-c-separation', 'rp_1_relu_1');
-% tempScriptReproDasgupta('measure-1-knn-perf', 'rp_1_relu_1');
+% tempScriptReproDasgupta('measure-c-separation', 'rp_1_relu_0');
+% tempScriptReproDasgupta('measure-1-knn-perf', 'rp_1_relu_0');
+tempScriptReproDasgupta('measure-c-separation', 'rp_1_relu_1');
+tempScriptReproDasgupta('measure-1-knn-perf', 'rp_1_relu_1');
 
 
 
