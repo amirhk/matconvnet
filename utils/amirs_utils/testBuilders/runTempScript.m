f656d24509a73c3516501a31c551ba00ab06868b
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-07-19 23:20:38 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -249,16 +249,25 @@
 fh_imdb_utils = imdbMultiClassUtils;
 
 dataset = 'cifar-multi-class-subsampled';
-posneg_balance = 'balanced-38';
+posneg_balance = 'balanced-50';
 [~, experiments] = setupExperimentsUsingProjectedImbds(dataset, posneg_balance, 0);
 % fh_imdb_utils.getImdbInfo(experiments{1}.imdb, 1);
 
-gpu = 1;
+gpu = 2;
 % [best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{1}.imdb, 'convV0P0RL0+fcV1-RF32CH3', gpu);
 % [best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{2}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
 % [best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{3}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
 [best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{4}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
 
+
+[best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{2}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
+[best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{3}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
+[best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{4}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
+
+
+[best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{2}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
+[best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{3}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
+[best_test_accuracy_mean, best_test_accuracy_std] = getSimpleTestAccuracyFromCnn(dataset, posneg_balance, experiments{4}.imdb, 'convV0P0RL0+fcV1-RF16CH64', gpu);
 
 
 
