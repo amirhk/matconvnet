1c35e6a1c9d5bec832c4b5dd404c21519e100a2e
Amir <a6karimi@v1020-wn-12-227.campus-dynamic.uwaterloo.ca>
2017-07-24 11:15:05 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -113,19 +113,19 @@ functionHandle = @tempScriptMeasureClassificationPerformance;
 % functionHandle('mnist-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
 functionHandle('cifar-multi-class-subsampled', 'balanced-50', '1-knn', 1);
-functionHandle('cifar-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
+% functionHandle('cifar-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('cifar-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
 functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', '1-knn', 1);
-functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
+% functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
 functionHandle('svhn-multi-class-subsampled', 'balanced-50', '1-knn', 1);
-functionHandle('svhn-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
+% functionHandle('svhn-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('svhn-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
 functionHandle('svhn-yes-white-multi-class-subsampled', 'balanced-50', '1-knn', 1);
-functionHandle('svhn-yes-white-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
+% functionHandle('svhn-yes-white-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('svhn-yes-white-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
 
