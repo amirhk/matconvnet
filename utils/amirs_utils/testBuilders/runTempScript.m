15f392c7ef407e8a00194e220267a991b02452dd
Amir <a6karimi@v1020-wn-12-227.campus-dynamic.uwaterloo.ca>
2017-07-25 15:43:31 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -140,7 +140,7 @@ functionHandle = @tempScriptMeasureClassificationPerformance;
 % functionHandle('imagenet-tiny-two-class-brown-bear-german-shepherd', 'balanced-50', '1-knn', 1);
 % functionHandle('imagenet-tiny-two-class-brown-bear-german-shepherd', 'balanced-500', '1-knn', 1);
 
-functionHandle('imagenet-tiny-two-class-school-bus-german-shepherd', 'balanced-50', '1-knn', 1);
+% functionHandle('imagenet-tiny-two-class-school-bus-german-shepherd', 'balanced-50', '1-knn', 1);
 functionHandle('imagenet-tiny-two-class-school-bus-german-shepherd', 'balanced-500', '1-knn', 1);
 
 
