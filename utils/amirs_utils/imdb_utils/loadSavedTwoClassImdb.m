120aa06de5eca4ed4ec0408c469517d8e3c6e40a
Amir <a6karimi@v1020-wn-12-227.campus-dynamic.uwaterloo.ca>
2017-07-25 15:18:04 -0400

    update

--- utils/amirs_utils/imdb_utils/loadSavedTwoClassImdb.m
+++ utils/amirs_utils/imdb_utils/loadSavedTwoClassImdb.m
@@ -105,6 +105,20 @@ function imdb = loadSavedTwoClassImdb(dataset, posneg_balance, fold_number, debu
           tmp = load(fullfile(path_to_imdbs, 'imagenet-tiny', 'balanced', 'saved-two-class-imagenet-tiny-pos73-neg192-balanced-500-500-train-500-500.mat'));
       end
 
+    case 'imagenet-tiny-two-class-school-bus-german-shepherd'
+      switch posneg_balance
+        case 'balanced-10'
+          tmp = load(fullfile(path_to_imdbs, 'imagenet-tiny', 'balanced', 'saved-two-class-imagenet-tiny-pos8-neg192-balanced-10-10-train-10-10.mat'));
+        case 'balanced-50'
+          tmp = load(fullfile(path_to_imdbs, 'imagenet-tiny', 'balanced', 'saved-two-class-imagenet-tiny-pos8-neg192-balanced-50-50-train-50-50.mat'));
+        case 'balanced-100'
+          tmp = load(fullfile(path_to_imdbs, 'imagenet-tiny', 'balanced', 'saved-two-class-imagenet-tiny-pos8-neg192-balanced-100-100-train-100-100.mat'));
+        case 'balanced-250'
+          tmp = load(fullfile(path_to_imdbs, 'imagenet-tiny', 'balanced', 'saved-two-class-imagenet-tiny-pos8-neg192-balanced-250-250-train-250-250.mat'));
+        case 'balanced-500'
+          tmp = load(fullfile(path_to_imdbs, 'imagenet-tiny', 'balanced', 'saved-two-class-imagenet-tiny-pos8-neg192-balanced-500-500-train-500-500.mat'));
+      end
+
     case 'cifar-two-class-deer-horse'
       % currently fold number is not implemented.
       switch posneg_balance
