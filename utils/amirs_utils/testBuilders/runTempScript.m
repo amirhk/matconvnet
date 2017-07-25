9c3f7e0750171a581242d528d44ab4bbe750015f
Amir <a6karimi@Amir-Hosseins-MacBook-Pro.local>
2017-07-24 23:37:18 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -135,6 +135,7 @@ functionHandle = @tempScriptMeasureClassificationPerformance;
 % functionHandle('svhn-yes-white-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
 functionHandle('imagenet-tiny-multi-class-subsampled', 'balanced-50', '1-knn', 1);
+functionHandle('imagenet-tiny-multi-class-subsampled', 'balanced-500', '1-knn', 1);
 
 
 
