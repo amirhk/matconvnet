9ebd4bbff3812797c5a0d22ed5b5e6d1d414d8e3
Amir <a6karimi@v1020-wn-12-227.campus-dynamic.uwaterloo.ca>
2017-07-28 19:49:05 -0400

    update

--- utils/amirs_utils/testBuilders/runTempScript.m
+++ utils/amirs_utils/testBuilders/runTempScript.m
@@ -112,26 +112,26 @@ functionHandle = @tempScriptMeasureClassificationPerformance;
 % functionHandle('cifar-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('cifar-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 
-functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', '1-knn', 1);
+% functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', '1-knn', 1);
 % functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 % functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-500', '1-knn', 1);
 % functionHandle('cifar-no-white-multi-class-subsampled', 'balanced-2500', '1-knn', 1);
 
-functionHandle('cifar-no-white-two-class-deer-truck', 'balanced-266', '1-knn', 1);
+% functionHandle('cifar-no-white-two-class-deer-truck', 'balanced-266', '1-knn', 1);
 
-functionHandle('stl-10-multi-class-subsampled', 'balanced-50', '1-knn', 1);
+% functionHandle('stl-10-multi-class-subsampled', 'balanced-50', '1-knn', 1);
 % functionHandle('stl-10-multi-class-subsampled', 'balanced-50', 'c-sep', 1);
 % functionHandle('stl-10-multi-class-subsampled', 'balanced-50', 'cnn', 1);
 % functionHandle('stl-10-multi-class-subsampled', 'balanced-500', '1-knn', 1);
 
-% functionHandle('imagenet-tiny-multi-class-subsampled', 'balanced-50', '1-knn', 1);
+functionHandle('imagenet-tiny-multi-class-subsampled', 'balanced-50', '1-knn', 1);
 % functionHandle('imagenet-tiny-multi-class-subsampled', 'balanced-500', '1-knn', 1);
 
-% functionHandle('imagenet-tiny-two-class-brown-bear-german-shepherd', 'balanced-50', '1-knn', 1);
+functionHandle('imagenet-tiny-two-class-brown-bear-german-shepherd', 'balanced-50', '1-knn', 1);
 % functionHandle('imagenet-tiny-two-class-brown-bear-german-shepherd', 'balanced-500', '1-knn', 1);
 
-% functionHandle('imagenet-tiny-two-class-school-bus-german-shepherd', 'balanced-50', '1-knn', 1);
+functionHandle('imagenet-tiny-two-class-school-bus-german-shepherd', 'balanced-50', '1-knn', 1);
 % functionHandle('imagenet-tiny-two-class-school-bus-german-shepherd', 'balanced-500', '1-knn', 1);
 
 % functionHandle('mnist-multi-class-subsampled', 'balanced-50', '1-knn', 1);
